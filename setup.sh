f1e2797e939247c9c17fb352dc9b83e22d9411dc
dtisoy <danieltisoy2001@gmail.com>
2024-01-19 13:02:37 -0500

    add: vim setup automation

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Vim Setup
+
+# add .vim and its folders
+mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
+# install plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# add color scheme from github
+cd ~/.vim/colors
+curl -o molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+# create symbolic links
+cd ~
+ln -s ~/MyLinuxConfig/myVim/.vimrc ~/.vimrc
+ln -s ~/MyLinuxConfig/myVim/.vim/config_files config_files
+
+echo "setup finished succesfully :)"
+echo "enter vim and execute :PlugInstall"
