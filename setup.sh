d12896572ddd49489b9a025e79a325ee357eb996
dtisoy <danieltisoy2001@gmail.com>
2024-01-19 13:54:24 -0500

    add: terminal theme instalation

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 # Vim Setup
 
 # add .vim and its folders
+echo "Setting Vim"
 mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
 # install plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -14,5 +15,11 @@ cd ~
 ln -s ~/MyLinuxConfig/myVim/.vimrc ~/.vimrc
 ln -s ~/MyLinuxConfig/myVim/.vim/config_files ~/.vim/config_files
 
+# Terminal Theme
+echo "Installing terminal theme"
+mkdir ./.local/bin && curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
+echo 'eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/neko.omp.json')"' >> ~/.profile
+. ~/.profile
+
+
 echo "setup finished succesfully :)"
-echo "enter vim and execute :PlugInstall"
