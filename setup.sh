4009223c74d90aa34ef1d27f2f79fad595d54cf2
dtisoy <danieltisoy2001@gmail.com>
2024-01-19 14:14:13 -0500

    fix: . ~/.profile doesn't work in the setup.sh

--- setup.sh
+++ setup.sh
@@ -20,8 +20,8 @@ echo "Installing terminal theme"
 mkdir ~/.local/bin && curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 echo 'eval "$(oh-my-posh init bash --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/neko.omp.json')"' >> ~/.profile
 
-. ~/.profile
 
 
 echo "setup finished succesfully :)"
-echo "restart the terminal"
+echo "run the next command: "
+echo ". ~/.profile"
